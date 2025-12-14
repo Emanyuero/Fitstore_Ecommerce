de57ebe7d9689b82130432a42d7b1cabf0a30068
Emmanuel Gamba <emmanuelgamba14324@gmail.com>
2025-12-14 12:47:20 +0800

    remove router link

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';   
+
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink, CommonModule],
+    imports: [ CommonModule],
     templateUrl: './header.html',
     styleUrl: './header.css'
 })
